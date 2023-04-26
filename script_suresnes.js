00917a9a15bdddda5102d3a2f02cfd0ad12f8f0f
Ambroise Pietu <pietuambroise@gmail.com>
2023-04-26 08:46:15 +0200

    homogenized indentation

--- script_suresnes.js
+++ script_suresnes.js
@@ -1,19 +1,18 @@
 const validCodes = ["AAAA"];
 
-        function checkCode() {
-            // Récupération du code entré par l'utilisateur
-            const input = document.getElementById("codeInput");
-            const code = input.value;
-
-            // Vérification si le code est valide
-            if (validCodes.includes(code)) {
-                // Affichage du message de victoire
-                const winMessage = document.getElementById("winMessage");
-                winMessage.classList.remove("hidden");
-                return false; // Empêche l'envoi du formulaire
-            } else {
-                const loseMessage = document.getElementById("loseMessage");
-                loseMessage.classList.remove("hidden");
-                return false; // Empêche l'envoi du formulaire
-            }
-        }
+function checkCode() {
+  const input = document.getElementById("codeInput");
+  // Récupération du code entré par l'utilisateur
+  const code = input.value;
+  if (validCodes.includes(code)) {
+    // Affichage du message de victoire
+    const winMessage = document.getElementById("winMessage");
+    winMessage.classList.remove("hidden");
+    return false; // Empêche l'envoi du formulaire
+  } else {
+    const loseMessage = document.getElementById("loseMessage");
+    loseMessage.classList.remove("hidden");
+    return false; // Empêche l'envoi du formulaire
+  }
+}
+// Vérification si le code est valide
